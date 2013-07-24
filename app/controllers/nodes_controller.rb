178035915e031f676bff6ddede20f6378341adad
sakurazuka <hiroshi.sakurazuka@gmail.com>
2013-07-24 13:49:21 +0900

    コメント追加

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,10 +1,19 @@
 class NodesController < ApplicationController
   def index
     @nodes = Node.roots
+
+    # ここで親階層扱いにしたい構成部品一覧を取得
+    # @nodes = EngineComponent.where(レベル２まで、アドレスあり、engine_no=current).order("address")
   end
 
   def children
     @nodes = Node.find(params[:id].to_i).children
+
+    # まずは選択された親要素を取得
+    # parent = EngineComponent.find(params[:id].to_i)
+
+    # 子供の要素を特定
+    # @nodes = EngineComponent.where(レベルが親要素＋１、アドレスに親要素のアドレスを含む、engine_no=current).order("address")
 
     render layout: false
   end
